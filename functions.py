fcbbbc688654d9ac9519a75977acf2660b5c3f9a
Sakib3602 <sakibsarker6969@gmail.com>
2025-08-22 15:18:49 +0600

    function and condition

---
+++ functions.py
@@ -0,0 +1,19 @@
+def no():
+    a = int(input("Enter a number: "))
+    print("You entered:", type(a), a)
+no()
+
+def condition():
+    b = input("Enter a character: ")
+    if b == "rahim":
+        print("You entered rahim")
+    elif b== "siam":
+        print("You entered siam")
+    elif b == "sakib":
+        print("You entered sakib")
+    elif b == "sabbir":
+        print("You entered sabbir")
+    else : 
+        print("You entered something else")
+condition()
+
