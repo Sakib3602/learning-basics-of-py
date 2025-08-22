44a9235d99737bd54d6c843ed3c5d6b51038f062
Sakib3602 <sakibsarker6969@gmail.com>
2025-08-22 14:31:10 +0600

    first hello world and explore type variable and input

---
+++ dataType.py
@@ -0,0 +1,28 @@
+# no need to declare the type.
+"""
+comment
+xsxcss
+scsc
+"""
+age = 120
+print(type(age))
+is_right = True
+print(type(is_right))
+
+percent = 12.132456787908764534
+
+print(type(percent))
+
+# type casting
+toI = int(percent)
+print(type(toI))
+
+age = float(age);
+print(type(age))
+
+# taking input from user
+do = input("give the input : ")
+print("hello" + " " +do)
+
+
+
