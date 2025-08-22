314f59384fff1088a80f6061731ac3e45b4c4434
Sakib3602 <sakibsarker6969@gmail.com>
2025-08-22 17:18:02 +0600

    modules import

---
+++ modules.py
@@ -0,0 +1,5 @@
+# here if i use * then all the function will be imported
+from functions import *  # Importing all functions from the 'functions' module
+# Importing specific functions from the 'functions' module
+# from functions import all_sum, all_sum_d, famous_name  # Importing specific functions  
+no()
