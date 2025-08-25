521a695fba34deb9c5e8508dbf91580297ee7866
Sakib3602 <sakibsarker6969@gmail.com>
2025-08-25 10:42:08 +0600

    set and object/dictionary

---
+++ set_object.py
@@ -0,0 +1,19 @@
+x = {"sakib", "rahim" , "karim"}
+
+x.add("lol")
+x.remove("sakib")
+print(x)
+
+
+# object / dictionary
+
+phone = {
+    "name" : "sakib",
+    "phone" : 1928383,
+    "age" : 24,
+    "job" : "student"
+}
+
+for i,j in phone.items():
+    print(f'here he value :: {i} and this is value result ::{j}')
+
