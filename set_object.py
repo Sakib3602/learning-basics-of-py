456348b22d96a81fdff7ac18654d9f7f7c369939
Sakib3602 <sakibsarker6969@gmail.com>
2025-08-25 10:46:42 +0600

    set and object/dictionary

--- set_object.py
+++ set_object.py
@@ -17,3 +17,22 @@ phone = {
 for i,j in phone.items():
     print(f'here he value :: {i} and this is value result ::{j}')
 
+
+phone = [{
+    "name" : "iphone",
+    "phone" : 1928383,
+    "ranf" : 24,
+    "job" : "student"
+},
+{
+    "name" : "samsung",
+    "phone" : 1928383,
+    "ranf" : 24,
+    "job" : "student"
+},
+]
+
+for i in phone:
+    for x,y in i.items():
+        print(f'vari :: {x} and value ::: {y}')
+
